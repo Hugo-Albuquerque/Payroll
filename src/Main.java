fe025510b0193e6ae0b0a3d193ac010eae8a815f
Hugo Ferreira de Albuquerque <45054092+Hugo-Albuquerque@users.noreply.github.com>
2018-11-21 18:32:13 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner menu = new Scanner(System.in);
+
+
+        System.out.println("MENU");
+        System.out.println("1 - Adicionar empregado ");
+        System.out.println("2 - Remover empregado ");
+        System.out.println("3 - Lançar cartao de ponto ");
+        System.out.println("4 - Lançar resultado de venda ");
+        System.out.println("5 - Lançar taxa de serviço ");
+        System.out.println("6 - Alterar dados do empregado ");
+        System.out.println("7 - Rodar folha de pagamento hoje");
+        System.out.println("8 - Desfazer ");
+        System.out.println("9 - Refazer ");
+        System.out.println("10 - Agenda de pagamento ");
+        System.out.println("11 - Criar nova agenda ");
+
+        System.out.println("Selecione a opcao");
+        int opcao = menu.nextInt();
+
+        switch(opcao)
+        {
+            case 1:
+            {
+                break;
+            }
+            case 2:
+            {
+                break;
+            }
+            case 3:
+            {
+                break;
+            }
+            case 4:
+            {
+                break;
+            }
+            case 5:
+            {
+                break;
+            }
+            case 6:
+            {
+                break;
+            }
+            case 7:
+            {
+                break;
+            }
+            case 8:
+            {
+                break;
+            }
+            case 9:
+            {
+                break;
+            }
+            case 10:
+            {
+                break;
+            }
+            case 11:
+            {
+                break;
+            }
+        }
+
+    }
+}
